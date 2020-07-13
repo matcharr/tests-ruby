275ef56bf36c83a7a5b6f07e89f0a0333bac44e8
matcharr <you@example.com>
2020-07-13 16:15:09 +0200

    test

--- lib/02_calculator.rb
+++ lib/02_calculator.rb
@@ -1,15 +1,23 @@
-def add(x,y)
-    x + y
-end
-
-def sum(number, other)
-    number + other
+def add(num1, num2)
+    num1 + num2
   end
-
-def substract
-
-end
-
-def multiply(x,y)
-    x * y
-end
+  
+  def substract(num1, num2)
+    num1 - num2
+  end
+  
+  def sum(num1, num2)
+    num1 + num2
+  end
+  
+  def multiply(num1, num2)
+    num1 * num2
+  end
+  
+  def factorial(num1, num2)
+    
+  end
+  
+  def power(num1, num2)
+    num1 ** num2
+  end
